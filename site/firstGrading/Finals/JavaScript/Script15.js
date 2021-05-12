0ec8e7b385b2deb3369465d51b423c5a80ca79d8
oliver1 <raddad417@gmail.com>
2021-05-12 08:59:37 +0300

    ahmed

---
+++ site/firstGrading/Finals/JavaScript/Script15.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById('name').value = "AL-GABRI, AHMED HUSSEIN";
+document.getElementById('phone').value = "09674052744";
+document.getElementById('address').value = "Lonilahill, Baguio city";
+document.getElementById('email').value = "20183755@s.ubaguio.edu";
+document.getElementById('program').value = "Computer Science";
+document.getElementById('idnmbr').value = "20183755";
+document.getElementById('year').value = "1st Year";
+document.getElementById('section').value = "IAA1";
+document.getElementById('age').value = "21";
+
+
